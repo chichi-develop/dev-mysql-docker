9d13f16d9bbdcef790612b07cc1d4e144f2687c8
miyamoto-yosuke <34563847+miyamoto-yosuke@users.noreply.github.com>
2019-01-23 13:56:21 +0900

    Update 001-create-tables.sql

--- docker/db/sql/001-create-tables.sql
+++ docker/db/sql/001-create-tables.sql
@@ -2,11 +2,11 @@
 -- drop table CM_ACLG cascade;
 
 create table CM_ACLG (
-  AL_IDACTV INT not null comment 'ID'
-  , AL_NMACTV VARCHAR(200) not null comment 'アクティビティ区分名'
+  AL_IDACTV INT not null AUTO_INCREMENT comment 'ID'
+  , AL_NMACTV VARCHAR(40) not null comment 'アクティビティ区分名'
   , AL_NOACTV INT not null comment 'アクティビティ番号'
   , AL_DTACTV DATE not null comment 'アクティビティ日付'
-  , AL_CDSQSK INT not null comment '請求先読者番号'
+  , AL_CDSQSK VARCHAR(8) comment '請求先読者番号'
   , AL_NMSQSK VARCHAR(200) not null comment '請求先顧客名'
   , AL_NMSQBU VARCHAR(200) comment '請求先部署名'
   , AL_NMSQTN VARCHAR(200) comment '請求先担当名'
@@ -14,15 +14,15 @@ create table CM_ACLG (
   , AL_SUSURY INT comment '数量'
   , AL_KGTNKA INT comment '単価'
   , AL_KGGOKE INT comment '合計金額'
-  , AL_TXBIKO VARCHAR(200) comment '備考'
-  , AL_CDCSTM VARCHAR(200) comment '読者番号'
+  , AL_TXBIKO VARCHAR(500) comment '備考'
+  , AL_CDCSTM VARCHAR(8) comment '読者番号'
   , AL_NMCSTM VARCHAR(200) comment '顧客名'
   , AL_NMTNBU VARCHAR(200) comment '部署名'
   , AL_NMTNTO VARCHAR(200) comment '担当者名'
   , constraint CM_ACLG_PKC primary key (AL_IDACTV)
 ) comment 'アクティビティログ' ;
 
-create unique index CM_ACLG_I01
+create index CM_ACLG_I01
   on CM_ACLG(AL_NMACTV,AL_CDSQSK);
 
 create index CM_ACLG_I02
