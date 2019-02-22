40a0a824f88ae3b8d647c1ffd7d160b26543a14a
TakeshiMouri <42727682+takeshimouri@users.noreply.github.com>
2019-02-22 16:26:28 +0900

    Update 001-create-tables.sql

--- docker/db/sql/001-create-tables.sql
+++ docker/db/sql/001-create-tables.sql
@@ -1,29 +1,25 @@
 -- アクティビティログ
--- drop table CM_ACLG cascade;
+--* BackupToTempTable
+drop table cm_aclg cascade;
 
-create table CM_ACLG (
-  AL_IDACTV INT not null AUTO_INCREMENT comment 'ID'
-  , AL_NMACTV VARCHAR(40) not null comment 'アクティビティ区分名'
-  , AL_NOACTV INT not null comment 'アクティビティ番号'
-  , AL_DTACTV DATE not null comment 'アクティビティ日付'
-  , AL_CDSQSK VARCHAR(8) comment '請求先読者番号'
-  , AL_NMSQSK VARCHAR(200) not null comment '請求先顧客名'
-  , AL_NMSQBU VARCHAR(200) comment '請求先部署名'
-  , AL_NMSQTN VARCHAR(200) comment '請求先担当名'
-  , AL_TXACTV VARCHAR(200) not null comment 'アクティビティ内容'
-  , AL_SUSURY INT comment '数量'
-  , AL_KGTNKA INT comment '単価'
-  , AL_KGGOKE INT comment '合計金額'
-  , AL_TXBIKO VARCHAR(500) comment '備考'
-  , AL_CDCSTM VARCHAR(8) comment '読者番号'
-  , AL_NMCSTM VARCHAR(200) comment '顧客名'
-  , AL_NMTNBU VARCHAR(200) comment '部署名'
-  , AL_NMTNTO VARCHAR(200) comment '担当者名'
-  , constraint CM_ACLG_PKC primary key (AL_IDACTV)
+--* RestoreFromTempTable
+create table cm_aclg (
+  al_idactv INT not null AUTO_INCREMENT comment 'ID'
+  , al_nmactv VARCHAR(40) not null comment 'アクティビティ区分名'
+  , al_noactv INT not null comment 'アクティビティ番号'
+  , al_dtactvAL DATE not null comment 'アクティビティ日付'
+  , al_cdsqsk VARCHAR(8) comment '請求先読者番号'
+  , al_nmsqsk VARCHAR(200) not null comment '請求先顧客名'
+  , al_nmsqbu VARCHAR(200) comment '請求先部署名'
+  , al_nmsqtn VARCHAR(200) comment '請求先担当名'
+  , al_txactv VARCHAR(200) not null comment 'アクティビティ内容'
+  , al_susury INT comment '数量'
+  , al_kgtnka INT comment '単価'
+  , al_kggoke INT comment '合計金額'
+  , al_txbiko VARCHAR(500) comment '備考'
+  , al_cdcstm VARCHAR(8) comment '読者番号'
+  , al_nmcstm VARCHAR(200) comment '顧客名'
+  , al_nmtnbu VARCHAR(200) comment '部署名'
+  , al_nmtnto VARCHAR(200) comment '担当者名'
+  , constraint cm_aclg_pkc primary key (al_idactv)
 ) comment 'アクティビティログ' ;
-
-create index CM_ACLG_I01
-  on CM_ACLG(AL_NMACTV,AL_CDSQSK);
-
-create index CM_ACLG_I02
-  on CM_ACLG(AL_DTACTV,AL_NMACTV,AL_CDSQSK);
